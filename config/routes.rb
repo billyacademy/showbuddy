26720278cd494f8ec670285ce1b9d5a2fb694d73
William Mahoney <wm.j.mahoney@gmail.com>
2015-01-04 13:59:56 -0500

    updated routes, tests passing to visit venue's show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-resources :venues, only: [:index]
+resources :venues, only: [:index, :show]
 
 root 'welcomes#index'
 end
---
+++ spec/features/venues/user_visits_venue_show_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature "a user visits the venues show page", %q(
+As a user
+I want to visit an individual venue's ShowBuddy page
+So that I can see upcoming, highlighted shows at the venue
+
+Acceptance Criteria
+
+[x] I must be able to navigate to an individual venue's page from the venues page
+[x] I must see detailed information specific to that venue
+
+) do
+  scenario "user visits a venue's page" do
+
+  new_venue = FactoryGirl.create(:venue)
+
+  visit venues_path
+  click_on "#{new_venue.name}'s ShowBuddy Page"
+
+  expect(page).to have_content new_venue.name
+  expect(page).to have_content new_venue.address_line_1
+  expect(page).to have_content "#{new_venue.name}'s Official Website"
+
+  end
+end
