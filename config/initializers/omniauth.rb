4da44526c85a9d72d1ba1600ca325802cfe306d4
William Mahoney <wm.j.mahoney@gmail.com>
2015-01-12 16:41:56 -0500

    added Twitter Authentication, User Model, Migration, updated routes, created sessions controller

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV[API_KEY], ENV[API_SECRET]
+  provider :twitter, 'SmCO5zCuN0zrQCzhkWw4A3ham', 'O6GSu1kTdkkc5PXAcdzpMm7pVhguGlE2TWyvRIGceNS1v3pG4G'
 end
