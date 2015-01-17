3e982484bd1ad923ef6bdb2818bcbdc3d3458544
William Mahoney <wm.j.mahoney@gmail.com>
2015-01-17 15:21:19 -0500

    refactored concert index, updated search test

--- spec/features/concerts/search_concerts_spec.rb
+++ spec/features/concerts/search_concerts_spec.rb
@@ -8,7 +8,7 @@ So that I can be informed on details of concerts I want to attend
 Acceptance Criteria
 [] A user must be able to search for a concert
   ) do
-  scenario "fill in search form and view results" do
+  scenario "fill in search form and view results", focus: true do
     the_sinclair = FactoryGirl.create(:venue, id: 2, name: "The Sinclair")
     paradise_rock_club = FactoryGirl.create(:venue, id: 3, name: "Paradise Rock Club")
     brand_new = FactoryGirl.create(:concert, artist_1: "Brand New", venue_id: 2)
@@ -16,13 +16,14 @@ Acceptance Criteria
     new_found_glory = FactoryGirl.create(:concert, artist_1: "New Found Glory" , venue_id: 3)
     moving_moutains = FactoryGirl.create(:concert, artist_1: "Moving Mountains", venue_id: 3)
 
-    visit venues_path
+    visit concerts_path
+
     fill_in "Search", with: "New"
     click_button "Search Concerts"
 
     expect(page).to have_link("New Found Glory", href: concert_path(new_found_glory))
     expect(page).to have_link("Brand New", href: concert_path(brand_new))
-    expect(page).to_not have_content("Against Me")
-    expect(page).to_not have_content("Moving Mountains")
+    expect(page).to_not have_content(new_found_glory.artist_1)
+    expect(page).to_not have_content(moving_moutains.artist_1)
   end
 end
