7c5a1e9ea8256aadfb82b46797369028d81a746b
William Mahoney <wm.j.mahoney@gmail.com>
2015-01-16 20:56:41 -0500

    added a validation on states, two character identifiers only

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -11,7 +11,10 @@ class Venue < ActiveRecord::Base
     presence: :true
 
   validates :state,
-    presence: :true
+    presence: :true, inclusion: { in: %w(AL AK AZ AR CA CO CT DE FL GA HI ID IL
+    IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA
+    RI SC SD TN TX UT VT VA WA WV WI WY), message: "Please enter the correct two
+    character state identifier"}
 
   validates :zip,
     presence: :true
