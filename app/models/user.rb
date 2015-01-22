ae85618c706ce6201285a21e59718efa31a516bc
William Mahoney <wm.j.mahoney@gmail.com>
2015-01-22 18:12:21 -0500

    added fonts and did more styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["info"]["nickname"]
+      user.name = auth["info"]["name"]
       user.image = auth["info"]["image"]
     end
   end
