e521e5e622f42a2a06ff13b7c5e2b52162a8cfe9
William Mahoney <wm.j.mahoney@gmail.com>
2015-01-27 13:37:59 -0500

    updated the message on state validations, more readable

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -17,7 +17,7 @@ class Venue < ActiveRecord::Base
     presence: :true,
     inclusion: { in: %w(AL AK AZ AR CA CO CT DE FL GA HI ID IL
     IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA
-    RI SC SD TN TX UT VT VA WA WV WI WY), message: "Please enter the correct two
+    RI SC SD TN TX UT VT VA WA WV WI WY), message: "is invalid, please enter the correct two
     character state identifier"}
 
   validates :zip,
