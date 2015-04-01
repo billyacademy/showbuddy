d22ebea494fc64d7f6d0f579f9d332cc6a2e6f3e
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-01 11:15:25 -0400

    made some changes, if image or nickname are updated on twitter, upon logging in showbuddy will recognize those changes.. should no longer get missing user images or old names

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path, notice: "Signed in!"
+    if env["omniauth.auth"]["info"]["image"] != user.image
+      user.image = env["omniauth.auth"]["info"]["image"]
+      user.save
+    end
+
+    if env["omniauth.auth"]["info"]["nickname"] != user.name
+      user.name = env["omniauth.auth"]["info"]["nickname"]
+      user.save
+    end
   end
 
   def destroy
